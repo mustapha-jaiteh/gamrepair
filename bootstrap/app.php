3ff75845239600f585a4b3895175a510bf626aaa
mustapha-jaiteh <neymarjaiteh@yahoo.com>
2026-03-31 15:25:51 +0000

    commit-middleware-update-31-03-26

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+        $middleware->trustProxies(at: '*');
+        // $middleware->web(append: [
+        //     \App\Http\Middleware\HandleInertiaRequests::class,
+        //     \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        // ]);
 
         //
     })
